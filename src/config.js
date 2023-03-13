ce7caf47f98a5fbcf6c804e8d3c36bd2788e3958
Rehman Ali <arehmanali.uet@gmail.com>
2023-03-13 18:37:32 +0500

    config changes

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export const config = {
   SERVER_URL:
     process.env.REACT_APP_NODE_ENV === "development"
       ? "http://localhost:8900"
-      : process.env.REACT_APP_SERVER_URL,
+      : "http://34.241.166.1:8900/api/v1",
 };
 
 console.log("SERVER_URL:", config.SERVER_URL);
